0bc4d02ab491a977bf798b9d8e0a09f9478bd238
tekofx <tekofxx@gmail.com>
2025-08-27 16:16:56 +0200

    Added more config parameters and created example.env

--- internal/configuration/config.go
+++ internal/configuration/config.go
@@ -9,6 +9,11 @@ import (
 )
 
 type AppConfiguration struct {
+	Ip      string
+	Port    string
+	Version string
+	ApiName string
+
 	SumAppToken string
 
 	CorsAccessControlAllowOrigin  string
@@ -28,6 +33,11 @@ func LoadConfig(env_file string) AppConfiguration {
 	config := AppConfiguration{
 		SumAppToken: os.Getenv("SUM_APP_TOKEN"),
 
+		Ip:      os.Getenv("IP"),
+		Port:    os.Getenv("PORT"),
+		Version: os.Getenv("VERSION"),
+		ApiName: os.Getenv("API_NAME"),
+
 		CorsAccessControlAllowOrigin:  os.Getenv("CORS_ORIGIN"),
 		CorsAccessControlAllowMethods: os.Getenv("CORS_METHODS"),
 		CorsAccessControlAllowHeaders: os.Getenv("CORS_HEADERS"),
@@ -37,6 +47,24 @@ func LoadConfig(env_file string) AppConfiguration {
 		JWTSecret:            os.Getenv("JWT_SECRET"),
 	}
 
+	setDefaultVariablesIfNeeded(&config)
 	return config
 
+}
+func setDefaultVariablesIfNeeded(configuration *AppConfiguration) {
+	if configuration.Ip == "" {
+		configuration.Ip = "127.0.0.1"
+	}
+
+	if configuration.Port == "" {
+		configuration.Ip = "8080"
+	}
+
+	if configuration.ApiName == "" {
+		configuration.Ip = "api"
+	}
+
+	if configuration.Version == "" {
+		configuration.Ip = "v1"
+	}
 }
